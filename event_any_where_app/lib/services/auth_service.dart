0584dccf8f56fef73ffcf69864c7d662a9068a66
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2024-11-27 23:10:17 +0700

    feat: create service for login func

--- event_any_where_app/lib/services/auth_service.dart
+++ event_any_where_app/lib/services/auth_service.dart
@@ -38,4 +38,68 @@ class AuthService {
       return false;
     }
   }
+
+  // Hàm đăng nhập
+  Future<Map<String, String>?> login(String email, String password) async {
+    final url = Uri.parse('$_baseUrl/login');
+    try {
+      final response = await http.post(
+        url,
+        body: jsonEncode({'email': email, 'password': password}),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final data = jsonDecode(response.body);
+        print(data);
+
+        final accessToken = data['tokenAccess'];
+        final refreshToken = data['tokenRefresh'];
+        final userInfo = data['user'];
+
+        if (accessToken != null && refreshToken != null && userInfo != null) {
+          if (userInfo['hobbies'] is List) {
+            userInfo['hobbies'] = (userInfo['hobbies'] as List).join(', ');
+          }
+
+          await saveTokens(accessToken, refreshToken);
+          await saveUserInfo(userInfo);
+          return {
+            'access_token': accessToken,
+            'refresh_token': refreshToken,
+          };
+        } else {
+          print('Tokens or user info not found in login response');
+          return null;
+        }
+      } else {
+        print('Login failed: ${response.statusCode} - ${response.body}');
+        return null;
+      }
+    } catch (error) {
+      print('Error during login: $error');
+      return null;
+    }
+  }
+
+  // Lưu access token và refresh token vào SharedPreferences
+  Future<void> saveTokens(String accessToken, String refreshToken) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('access_token', accessToken);
+    await prefs.setString('refresh_token', refreshToken);
+  }
+
+  // Lưu thông tin người dùng vào SharedPreferences
+  Future<void> saveUserInfo(Map<String, dynamic> userInfo) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user_id', userInfo['_id']);
+    await prefs.setString('name', userInfo['name'] ?? '');
+    await prefs.setString('email', userInfo['email'] ?? '');
+    await prefs.setString('role', userInfo['role'] ?? '');
+    await prefs.setString('image', userInfo['image'] ?? '');
+    await prefs.setString('phone', userInfo['phone'] ?? '');
+    await prefs.setString('description', userInfo['description'] ?? '');
+    await prefs.setString('address', userInfo['address'] ?? '');
+    await prefs.setString('hobbies', userInfo['hobbies'] ?? '');
+  }
 }
