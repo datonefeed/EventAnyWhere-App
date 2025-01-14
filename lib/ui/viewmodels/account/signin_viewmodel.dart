0b3afdb2c3e7448cae08088e3c9da66389a510ae
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-01-14 22:38:55 +0700

    refactor: improve error handling

--- lib/ui/viewmodels/account/signin_viewmodel.dart
+++ lib/ui/viewmodels/account/signin_viewmodel.dart
@@ -2,7 +2,6 @@ import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:event_any_where_app/services/auth_service.dart';
 import 'package:event_any_where_app/ui/viewmodels/event/event_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:event_any_where_app/services/auth_service.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -14,57 +13,58 @@ class SignInViewModel extends ChangeNotifier {
 
   Future<void> login(
       BuildContext context, String email, String password) async {
-    isLoading = true;
+    _setLoadingState(true);
     errorMessage = '';
-    notifyListeners();
 
     try {
       final tokens = await _authService.login(email, password);
-
       if (tokens != null) {
-        if (rememberMe) {
-          await _authService.saveTokens(
-              tokens['access_token']!, tokens['refresh_token']!);
-        }
-
-        final snackBar = SnackBar(
-          content: AwesomeSnackbarContent(
-            title: 'Success!',
-            message: 'Login successfully',
-            contentType: ContentType.success,
-          ),
-          behavior: SnackBarBehavior.floating,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-        Provider.of<EventViewModel>(context, listen: false).fetchEvents();
-        context.go('/entryPoint');
+        await _handleLoginSuccess(context, tokens);
       }
     } catch (error) {
-      // `error` here is already the error message string from `AuthService`
-      errorMessage = error as String;
-
-      final snackBar = SnackBar(
-        content: AwesomeSnackbarContent(
-          title: 'Failure!',
-          message: 'Login Failed! $errorMessage',
-          contentType: ContentType.failure,
-        ),
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      );
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      errorMessage = error.toString();
+      _showSnackBar(context, 'Failure!', 'Login Failed! $errorMessage',
+          ContentType.failure);
+    } finally {
+      _setLoadingState(false);
     }
-
-    isLoading = false;
-    notifyListeners();
   }
 
   void toggleRememberMe() {
     rememberMe = !rememberMe;
     notifyListeners();
+  }
+
+  void _setLoadingState(bool state) {
+    isLoading = state;
+    notifyListeners();
+  }
+
+  Future<void> _handleLoginSuccess(
+      BuildContext context, Map<String, String> tokens) async {
+    if (rememberMe) {
+      await _authService.saveTokens(
+          tokens['access_token']!, tokens['refresh_token']!);
+    }
+
+    _showSnackBar(
+        context, 'Success!', 'Login successfully', ContentType.success);
+    Provider.of<EventViewModel>(context, listen: false).fetchEvents();
+    context.go('/entryPoint');
+  }
+
+  void _showSnackBar(
+      BuildContext context, String title, String message, ContentType type) {
+    final snackBar = SnackBar(
+      content: AwesomeSnackbarContent(
+        title: title,
+        message: message,
+        contentType: type,
+      ),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
