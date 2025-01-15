06ec8a6f4e0693adc746f93e462bc20874b21590
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-01-15 19:49:03 +0700

    refactor: optimize and enhance EventService functionality

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -10,18 +10,14 @@ class EventService {
   final String _categoriesUrl = '${ApiConstants.baseApiUrl}/category';
   final String _apiUrl = '${ApiConstants.baseApiUrl}/event/detail';
 
-  // Hàm lấy token từ SharedPreferences
   Future<String?> _getAccessToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('access_token');
   }
 
-  // Hàm xóa sự kiện
   Future<void> deleteEvent(String eventId) async {
     try {
-      // Lấy access_token từ SharedPreferences
-      final prefs = await SharedPreferences.getInstance();
-      final accessToken = prefs.getString('access_token');
+      final accessToken = await _getAccessToken();
       if (accessToken == null) {
         throw Exception("Access token is missing");
       }
@@ -29,83 +25,77 @@ class EventService {
       final response = await http.delete(
         Uri.parse('$_baseUrl/delete/$eventId'),
         headers: {
-          'Authorization':
-              'Bearer $accessToken', // Thêm access token vào header
-          'Content-Type': 'application/json', // Thêm content type nếu cần
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json',
         },
       );
 
-      if (response.statusCode == 200) {
-        // Xóa sự kiện thành công
-        print("Event deleted successfully");
-      } else {
-        // Nếu có lỗi xảy ra từ phía server
+      if (response.statusCode != 200) {
         throw Exception('Failed to delete event');
       }
     } catch (e) {
-      print('Error: $e');
-      throw Exception('Failed to delete event');
+      throw Exception('Failed to delete event: $e');
     }
   }
 
-  // Lấy thông tin sự kiện theo ID
   Future<Map<String, dynamic>> getEventById(String eventId) async {
-    final response = await http.get(Uri.parse('$_apiUrl/$eventId'));
+    try {
+      final response = await http.get(Uri.parse('$_apiUrl/$eventId'));
 
-    if (response.statusCode == 200) {
-      // Parse dữ liệu từ API thành Map
-      final data = json.decode(response.body);
-      return data['event']; // Trả về thông tin sự kiện
-    } else {
-      throw Exception('Failed to load event details');
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        if (data is Map<String, dynamic> && data.containsKey('event')) {
+          return data['event'] as Map<String, dynamic>;
+        } else {
+          throw Exception('Invalid response format.');
+        }
+      } else {
+        throw Exception('Failed to load event details: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error loading event details: $e');
     }
   }
 
-  // Phương thức lấy danh sách sự kiện
   Future<List<Map<String, dynamic>>> getEventsByCurrentUser() async {
-    final url = Uri.parse('$_baseUrl/organizer');
-    final accessToken = await _getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception("Access token not found");
-    }
-
     try {
+      final accessToken = await _getAccessToken();
+      if (accessToken == null) {
+        throw Exception("Access token not found");
+      }
+
       final response = await http.get(
-        url,
+        Uri.parse('$_baseUrl/organizer'),
         headers: {
           'Authorization': 'Bearer $accessToken',
           'Content-Type': 'application/json',
         },
       );
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
+
       if (response.statusCode == 200) {
-        final responseBody = jsonDecode(response.body);
-        final events = responseBody['events'] as List<dynamic>;
-        return events
-            .map((event) => event as Map<String, dynamic>)
-            .toList(); // Trả về danh sách sự kiện
+        final data = jsonDecode(response.body);
+        if (data is Map<String, dynamic> && data.containsKey('events')) {
+          return List<Map<String, dynamic>>.from(data['events']);
+        } else {
+          throw Exception('Invalid response format.');
+        }
       } else {
         throw Exception('Failed to fetch events: ${response.statusCode}');
       }
-    } catch (error) {
-      throw Exception('Error fetching events: $error');
+    } catch (e) {
+      throw Exception('Error fetching events: $e');
     }
   }
 
-  // Phương thức lấy danh sách danh mục sự kiện
   Future<List<Map<String, dynamic>>> getAllCategories() async {
-    final url = Uri.parse('$_categoriesUrl/list');
-    final accessToken = await _getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception("Access token not found");
-    }
-
     try {
+      final accessToken = await _getAccessToken();
+      if (accessToken == null) {
+        throw Exception("Access token not found");
+      }
+
       final response = await http.get(
-        url,
+        Uri.parse('$_categoriesUrl/list'),
         headers: {
           'Authorization': 'Bearer $accessToken',
           'Content-Type': 'application/json',
@@ -113,16 +103,16 @@ class EventService {
       );
 
       if (response.statusCode == 200) {
-        return List<Map<String, dynamic>>.from(jsonDecode(response.body));
+        final data = jsonDecode(response.body);
+        return List<Map<String, dynamic>>.from(data);
       } else {
         throw Exception('Failed to fetch categories: ${response.statusCode}');
       }
-    } catch (error) {
-      throw Exception('Error fetching categories: $error');
+    } catch (e) {
+      throw Exception('Error fetching categories: $e');
     }
   }
 
-  // Phương thức tạo sự kiện
   Future<bool> createEvent({
     required String title,
     required String description,
@@ -132,47 +122,41 @@ class EventService {
     required List<File> images,
     double price = 0,
   }) async {
-    final url = Uri.parse('$_baseUrl/add');
-    final accessToken = await _getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception("Access token not found");
-    }
-
-    final request = http.MultipartRequest('POST', url)
-      ..headers['Authorization'] = 'Bearer $accessToken'
-      ..fields['title'] = title
-      ..fields['description'] = description
-      ..fields['location'] = location
-      ..fields['date'] = date
-      ..fields['category_id'] = categoryId;
-
-    for (final image in images) {
-      request.files.add(await http.MultipartFile.fromPath(
-        'images',
-        image.path,
-        filename: basename(image.path),
-      ));
-    }
-
     try {
+      final accessToken = await _getAccessToken();
+      if (accessToken == null) {
+        throw Exception("Access token not found");
+      }
+
+      final request = http.MultipartRequest('POST', Uri.parse('$_baseUrl/add'))
+        ..headers['Authorization'] = 'Bearer $accessToken'
+        ..fields['title'] = title
+        ..fields['description'] = description
+        ..fields['location'] = location
+        ..fields['date'] = date
+        ..fields['category_id'] = categoryId;
+
+      for (final image in images) {
+        request.files.add(await http.MultipartFile.fromPath(
+          'images',
+          image.path,
+          filename: basename(image.path),
+        ));
+      }
+
       final response = await request.send();
 
       if (response.statusCode == 201) {
-        print('Event created successfully');
         return true;
       } else {
         final responseBody = await response.stream.bytesToString();
-        print('Failed to create event. Response: $responseBody');
-        return false;
+        throw Exception('Failed to create event: $responseBody');
       }
-    } catch (error) {
-      print('Error creating event: $error');
-      throw Exception('Error creating event: $error');
+    } catch (e) {
+      throw Exception('Error creating event: $e');
     }
   }
 
-  // Phương thức update sự kiện
   Future<bool> updateEvent({
     required String id,
     required String title,
@@ -181,42 +165,38 @@ class EventService {
     required String date,
     String? imagePath,
   }) async {
-    final url = Uri.parse('$_baseUrl/update/$id');
-    final accessToken = await _getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception("Access token not found");
-    }
-
-    final request = http.MultipartRequest('PUT', url)
-      ..headers['Authorization'] = 'Bearer $accessToken'
-      ..fields['title'] = title
-      ..fields['description'] = description
-      ..fields['location'] = location
-      ..fields['date'] = date;
-
-    if (imagePath != null && imagePath.isNotEmpty) {
-      request.files.add(await http.MultipartFile.fromPath(
-        'images',
-        imagePath,
-        filename: basename(imagePath),
-      ));
-    }
-
     try {
+      final accessToken = await _getAccessToken();
+      if (accessToken == null) {
+        throw Exception("Access token not found");
+      }
+
+      final request =
+          http.MultipartRequest('PUT', Uri.parse('$_baseUrl/update/$id'))
+            ..headers['Authorization'] = 'Bearer $accessToken'
+            ..fields['title'] = title
+            ..fields['description'] = description
+            ..fields['location'] = location
+            ..fields['date'] = date;
+
+      if (imagePath != null && imagePath.isNotEmpty) {
+        request.files.add(await http.MultipartFile.fromPath(
+          'images',
+          imagePath,
+          filename: basename(imagePath),
+        ));
+      }
+
       final response = await request.send();
 
       if (response.statusCode == 200) {
-        print('Event updated successfully');
         return true;
       } else {
         final responseBody = await response.stream.bytesToString();
-        print('Failed to update event. Response: $responseBody');
-        return false;
+        throw Exception('Failed to update event: $responseBody');
       }
-    } catch (error) {
-      print('Error updating event: $error');
-      throw Exception('Error updating event: $error');
+    } catch (e) {
+      throw Exception('Error updating event: $e');
     }
   }
 }
