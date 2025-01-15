982c64dccf67a8b27643d1bc5f6b583c5a256fc9
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-01-15 19:46:31 +0700

    fix: enhance EventSearchService with better error handling and validation

--- lib/services/event_search_service.dart
+++ lib/services/event_search_service.dart
@@ -5,17 +5,26 @@ class EventSearchService {
   final String baseUrl = 'http://10.0.2.2:3000/home/search';
 
   Future<List<dynamic>> searchEvents(String keyword) async {
+    if (keyword.isEmpty) {
+      return [];
+    }
+
     try {
-      // Thêm `keyword` vào query của URL
-      final uri = Uri.parse('$baseUrl?keyword=$keyword');
+      final uri =
+          Uri.parse('$baseUrl?keyword=${Uri.encodeQueryComponent(keyword)}');
       final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        return data['events'] ?? [];
+        if (data is Map<String, dynamic> && data.containsKey('events')) {
+          return data['events'] as List<dynamic>;
+        } else {
+          throw Exception('Invalid response format.');
+        }
       } else {
         throw Exception(
-            'Failed to fetch events. Status Code: ${response.statusCode}');
+          'Failed to fetch events. Status Code: ${response.statusCode}',
+        );
       }
     } catch (e) {
       throw Exception('Error fetching events: $e');
