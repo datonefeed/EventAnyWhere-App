7af411656789ea2960685d6a45b76670dca33035
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-01-20 21:21:45 +0700

    refactor: remove comment

--- lib/ui/viewmodels/home/filter_viewmodel.dart
+++ lib/ui/viewmodels/home/filter_viewmodel.dart
@@ -3,53 +3,37 @@ import 'package:event_any_where_app/models/event_model.dart';
 import 'package:event_any_where_app/services/filter_service.dart';
 
 class FilterViewModel extends ChangeNotifier {
-  final FilterService _eventService;
-
-  FilterViewModel(this._eventService);
+  final FilterService _eventService = FilterService();
 
   List<EventModel> _eventList = [];
-  String? _errorMessage;
+  String _error = "";
   bool _isLoading = false;
 
   List<EventModel> get eventList => _eventList;
-  String? get errorMessage => _errorMessage;
+  String get error => _error;
   bool get isLoading => _isLoading;
-
-  void _setLoading(bool value) {
-    _isLoading = value;
-    notifyListeners();
-  }
-
-  void _setError(String? message) {
-    _errorMessage = message;
-    notifyListeners();
-  }
-
-  void _setEvents(List<EventModel> events) {
-    _eventList = events;
-    notifyListeners();
-  }
 
   Future<void> fetchFilteredEvents({
     String? categoryId,
     String? dateOption,
     String? location,
   }) async {
-    _setLoading(true);
-    _setError(null);
+    _isLoading = true;
+    _error = ""; // Xóa lỗi cũ
+    notifyListeners();
 
     try {
-      final events = await _eventService.fetchFilteredEvents(
+      _eventList = await _eventService.fetchFilteredEvents(
         categoryId: categoryId ?? "",
         dateOption: dateOption ?? "",
         location: location ?? "",
       );
-      _setEvents(events);
     } catch (e) {
-      _setError("An error occurred while fetching events. Please try again.");
-      _setEvents([]);
+      _error = "Failed to fetch events: $e"; // Lưu lỗi
+      _eventList = [];
     } finally {
-      _setLoading(false);
+      _isLoading = false;
+      notifyListeners();
     }
   }
 }
