8d1b4c0350b0babae13162281653aaaeacf30a34
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-01-22 21:30:17 +0700

    refactor: Optimize PreviewScreen: code refactoring and comment removal

--- lib/ui/views/event/preview_screen.dart
+++ lib/ui/views/event/preview_screen.dart
@@ -11,7 +11,7 @@ class PreviewScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "3 of 4: Preview",
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
@@ -19,225 +19,215 @@ class PreviewScreen extends StatelessWidget {
         backgroundColor: MyTheme.backgroundcolor,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            GoRouter.of(context).pop();
-          },
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => GoRouter.of(context).pop(),
         ),
       ),
       body: Container(
         color: MyTheme.backgroundcolor,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildProgressBar(),
+            const SizedBox(height: 16),
+            _buildEventImage(context, eventViewModel),
+            const SizedBox(height: 16),
+            _buildEventDetails(eventViewModel),
+            const Spacer(),
+            _buildNextButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProgressBar() {
+    return Container(
+      height: 6,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(3),
+      ),
+      child: FractionallySizedBox(
+        alignment: Alignment.centerLeft,
+        widthFactor: 0.75,
+        child: Container(
+          decoration: BoxDecoration(
+            color: MyTheme.primaryColor,
+            borderRadius: BorderRadius.circular(3),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventImage(BuildContext context, EventViewModel eventViewModel) {
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.grey[800],
+          ),
+          child: eventViewModel.eventImages.isNotEmpty
+              ? Image.file(
+                  eventViewModel.eventImages.first,
+                  fit: BoxFit.cover,
+                )
+              : const Center(
+                  child: Icon(
+                    Icons.image,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                ),
+        ),
+        Positioned(
+          bottom: 8,
+          right: 8,
+          child: GestureDetector(
+            onTap: () => GoRouter.of(context).pop(),
+            child: Container(
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color: MyTheme.primaryColor,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Row(
+                children: const [
+                  Icon(Icons.edit, color: Colors.white, size: 16),
+                  SizedBox(width: 4),
+                  Text(
+                    "Edit Details",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEventDetails(EventViewModel eventViewModel) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          eventViewModel.title ?? "Event Title",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 16),
+        _buildIconTextRow(
+            Icons.calendar_today, eventViewModel.date ?? "Date Not Set"),
+        const SizedBox(height: 8),
+        _buildIconTextRow(
+            Icons.access_time, eventViewModel.time ?? "Time Not Set"),
+        const SizedBox(height: 8),
+        GestureDetector(
+          onTap: () {},
+          child: Row(
             children: [
-              Container(
-                height: 6,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(3),
-                ),
-                child: FractionallySizedBox(
-                  alignment: Alignment.centerLeft,
-                  widthFactor: 0.75,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: MyTheme.primaryColor,
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              // Hình ảnh sự kiện
-              Stack(
-                children: [
-                  Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.grey[800],
-                    ),
-                    child: eventViewModel.eventImages.isNotEmpty
-                        ? Image.file(
-                            eventViewModel.eventImages.first,
-                            fit: BoxFit.cover,
-                          )
-                        : Center(
-                            child: Icon(
-                              Icons.image,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                          ),
-                  ),
-                  Positioned(
-                    bottom: 8,
-                    right: 8,
-                    child: GestureDetector(
-                      onTap: () {
-                        GoRouter.of(context).pop();
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(6),
-                        decoration: BoxDecoration(
-                          color: MyTheme.primaryColor,
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.edit, color: Colors.white, size: 16),
-                            SizedBox(width: 4),
-                            Text(
-                              "Edit Details",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              // Tiêu đề sự kiện
+              Icon(Icons.add, color: MyTheme.primaryColor, size: 16),
+              const SizedBox(width: 8),
               Text(
-                eventViewModel.title ?? "Event Title",
+                "Add to Calendar",
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16),
-              // Ngày giờ
-              Row(
-                children: [
-                  Icon(Icons.calendar_today, color: Colors.grey, size: 16),
-                  SizedBox(width: 8),
-                  Text(
-                    eventViewModel.date ?? "Date Not Set",
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Icon(Icons.access_time, color: Colors.grey, size: 16),
-                  SizedBox(width: 8),
-                  Text(
-                    eventViewModel.time ?? "Time Not Set",
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              GestureDetector(
-                onTap: () {
-                  // Mở chức năng thêm vào lịch
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.add, color: MyTheme.primaryColor, size: 16),
-                    SizedBox(width: 8),
-                    Text(
-                      " Add to Calendar",
-                      style: TextStyle(
-                          color: MyTheme.primaryColor,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              // Địa điểm
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.location_on, color: Colors.grey, size: 16),
-                  SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      eventViewModel.location ?? "Location Not Set",
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              // Tên người tổ chức
-              Text(
-                "Organizer",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                eventViewModel.organizerName ?? "Organizer Not Set",
-                style: TextStyle(
-                  color: Colors.white,
+                  color: MyTheme.primaryColor,
                   fontSize: 16,
-                ),
-              ),
-              SizedBox(height: 16),
-              // Mô tả sự kiện
-              Text(
-                "Event Description",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
                   fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                eventViewModel.description ??
-                    "Description Not Set. Please go back and provide details.",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  height: 1.5,
-                ),
-              ),
-              Spacer(),
-              // Nút Next
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.push('/reviewAndSend');
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Next: Review & Send",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                      SizedBox(width: 8),
-                      Image.asset('assets/icons/right_arrow.png')
-                    ],
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: MyTheme.primaryColor,
-                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
                 ),
               ),
             ],
+          ),
+        ),
+        const SizedBox(height: 16),
+        _buildIconTextRow(
+            Icons.location_on, eventViewModel.location ?? "Location Not Set"),
+        const SizedBox(height: 16),
+        const Text(
+          "Organizer",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          eventViewModel.organizerName ?? "Organizer Not Set",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 16),
+        const Text(
+          "Event Description",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          eventViewModel.description ??
+              "Description Not Set. Please go back and provide details.",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            height: 1.5,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildIconTextRow(IconData icon, String text) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.grey, size: 16),
+        const SizedBox(width: 8),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.grey, fontSize: 16),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNextButton(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () => context.push('/reviewAndSend'),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Next: Review & Send",
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+            const SizedBox(width: 8),
+            Image.asset('assets/icons/right_arrow.png')
+          ],
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: MyTheme.primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
           ),
         ),
       ),
